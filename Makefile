ce664cd8d68f8c34190e3fcbe5ba9ee32204a880
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2020-08-14 00:31:38 +0200

    Add setup.py to create PyPi package

--- Makefile
+++ Makefile
@@ -2,4 +2,20 @@
 test:
 	@python3 -W ignore::DeprecationWarning -m unittest discover -p *test.py
 
-.PHONY : test
+dist: clean
+	@rm -rf dist build
+	@python3 setup.py sdist bdist_wheel
+
+install:
+	@python3 setup.py install --record uninstall.txt
+
+install-user:
+	@python3 setup.py install --user
+
+upload: dist
+	@twine upload --skip-existing dist/*
+
+clean:
+	@rm -rf dist build modm_devices.egg-info
+
+.PHONY : test dist install install-user upload clean
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name = "modm-devices",
+    version = "0.1.1",
+    python_requires=">=3.5.0",
+    packages = find_packages(exclude=["test"]),
+    package_data = {
+        "": ["resources/devices/*/*",
+             "resources/*",
+             "resources/*/*"],
+    },
+
+    install_requires = ["lxml"],
+
+    # Metadata
+    author = "Niklas Hauser",
+    author_email = "niklas@salkinium.com",
+    description = "Curated data for AVR and ARM Cortex-M devices",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license = "MPL-2.0",
+    keywords = "modm lbuild modm-devices stm32 avr sam nrf",
+    url = "https://github.com/modm-io/modm-devices",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Database",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+)
