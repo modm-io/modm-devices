f7eba50c48b4e3890016449641ec8e62439948fe
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2025-06-01 15:43:23 +0200

    [dfg] Generate RP2350 device data

--- tools/generator/raw-data-extractor/extract-rp.py
+++ tools/generator/raw-data-extractor/extract-rp.py
@@ -4,7 +4,8 @@ import shutil
 import os
 
 svdurls = [
-    ["rp2040.svd","https://github.com/raspberrypi/pico-sdk/raw/master/src/rp2040/hardware_regs/RP2040.svd"]
+    ["rp2040.svd","https://github.com/raspberrypi/pico-sdk/raw/master/src/rp2040/hardware_regs/RP2040.svd"],
+    ["rp2350.svd","https://github.com/raspberrypi/pico-sdk/raw/master/src/rp2350/hardware_regs/RP2350.svd"]
 ]
 
 shutil.rmtree("../raw-device-data/rp-devices", ignore_errors=True)
